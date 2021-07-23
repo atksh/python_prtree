50840788f46fa2a874816f6aeda9b05c9da58f99
kota_ishizuka <ishizuka.k.ad@m.titech.ac.jp>
2021-07-23 13:01:42 +0900

    fix UT

--- tests/test_PRTree.py
+++ tests/test_PRTree.py
@@ -5,7 +5,7 @@ import numpy as np
 
 def has_intersect(x, y, dim):
     return all(
-        [max(x[2 * i], y[2 * i]) <= min(x[2 * i + 1], y[2 * i + 1]) for i in range(dim)]
+        [max(x[i], y[i]) <= min(x[i + dim], y[i + dim]) for i in range(dim)]
     )
 
 
@@ -15,7 +15,7 @@ def test_result(PRTree, dim):
     idx = np.arange(100)
     x = np.random.rand(len(idx), 2 * dim)
     for i in range(dim):
-        x[:, 2 * i + 1] += x[:, 2 * i]
+        x[:, i + dim] += x[:, i]
 
     prtree = PRTree(idx, x)
     out = prtree.batch_query(x)
@@ -30,7 +30,7 @@ def test_io(PRTree, dim, tmp_path):
     idx = np.arange(100)
     x = np.random.rand(len(idx), 2 * dim)
     for i in range(dim):
-        x[:, 2 * i + 1] += x[:, 2 * i]
+        x[:, i + dim] += x[:, i]
 
     prtree = PRTree(idx, x)
 
@@ -53,23 +53,29 @@ def test_io(PRTree, dim, tmp_path):
         assert set(out[i]) == set(tmp)
 
 
+@pytest.mark.parametrize("from_scratch", [False])
 @pytest.mark.parametrize("PRTree, dim", [(PRTree2D, 2), (PRTree3D, 3)])
-def test_insert_erase(PRTree, dim):
+def test_insert_erase(from_scratch, PRTree, dim):
     np.random.seed(1)
     N = 10000
     idx = np.arange(N)
     x = np.random.rand(N, 2 * dim)
     for i in range(dim):
-        x[:, 2 * i + 1] = x[:, 2 * i] + x[:, 2 * i + 1] / np.sqrt(N) / 100
+        x[:, i + dim] = x[:, i] + x[:, i + dim] / np.sqrt(N) / 100
     prtree1 = PRTree(idx, x)
 
-    prtree2 = PRTree(idx[: N // 2], x[: N // 2])
-    for i in range(N // 2, N):
-        prtree2.insert(idx[i], x[i])
+    if from_scratch:
+        prtree2 = PRTree()
+        for i in range(N):
+            prtree2.insert(idx[i], x[i])
+    else:
+        prtree2 = PRTree(idx[: N // 2], x[: N // 2])
+        for i in range(N // 2, N):
+            prtree2.insert(idx[i], x[i])
 
     x = np.random.rand(100, 2 * dim)
     for i in range(dim):
-        x[:, 2 * i + 1] = x[:, 2 * i] + x[:, 2 * i + 1] / np.sqrt(N) / 100
+        x[:, i + dim] = x[:, i] + x[:, i + dim] / np.sqrt(N) / 100
     for i in range(x.shape[0]):
         assert set(prtree1.query(x[i])) == set(prtree2.query(x[i]))
 
@@ -78,6 +84,6 @@ def test_insert_erase(PRTree, dim):
         prtree2.erase(i)
 
     for i in range(dim):
-        x[:, 2 * i + 1] = x[:, 2 * i] + x[:, 2 * i + 1] / np.sqrt(N) / 100
+        x[:, i + dim] = x[:, i] + x[:, i + dim] / np.sqrt(N) / 100
     for i in range(x.shape[0]):
         assert set(prtree1.query(x[i])) == set(prtree2.query(x[i]))
