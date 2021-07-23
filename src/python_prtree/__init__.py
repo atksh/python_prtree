f4c45eef65958fc85f5d8fe4fa072e06b2384918
kota_ishizuka <ishizuka.k.ad@m.titech.ac.jp>
2021-07-23 23:27:57 +0900

    mod

--- src/python_prtree/__init__.py
+++ src/python_prtree/__init__.py
@@ -1,4 +1,27 @@
+import codecs
+import pickle
+
 from .PRTree import _PRTree2D, _PRTree3D
+
+__all__ = [
+    "PRTree2D",
+    "PRTree3D",
+]
+
+
+def dumps(obj):
+    if obj is None:
+        return None
+    else:
+        return pickle.dumps(obj)
+
+
+def loads(s):
+    if s is None:
+        return None
+    else:
+        return pickle.loads(s)
+
 
 class PRTree2D:
     Klass = _PRTree2D
@@ -10,12 +33,13 @@ class PRTree2D:
         def handler_function(*args, **kwargs):
             ret = getattr(self._tree, name)(*args, **kwargs)
             return ret
+
         return handler_function
 
     @property
     def n(self):
         return self._tree.size()
-    
+
     def __len__(self):
         return self.n
 
@@ -24,17 +48,33 @@ class PRTree2D:
             raise ValueError("Nothing to erase")
         self._tree.erase(idx)
 
-    def insert(self, idx, bb):
+    def set_obj(self, idx, obj):
+        objdumps = dumps(obj)
+        self._tree.set_obj(idx, objdumps)
+
+    def get_obj(self, idx):
+        obj = self._tree.get_obj(idx)
+        return loads(obj)
+
+    def insert(self, idx, bb, obj=None):
+        objdumps = dumps(obj)
         if self.n == 0:
             self._tree = self.Klass([idx], [bb])
+            self._tree.set_obj(idx, objdumps)
         else:
-            self._tree.insert(idx, bb)
-    
-    def query(self, *args):
+            self._tree.insert(idx, bb, objdumps)
+
+    def query(self, *args, return_obj=False):
         if len(args) == 1:
-            return self._tree.query(*args)
+            out, objs = self._tree.query(*args)
         else:
-            return self._tree.query(args)
+            out, objs = self._tree.query(args)
+        if return_obj:
+            objs = [loads(obj) for obj in objs]
+            return objs
+        else:
+            return out
+
 
 class PRTree3D(PRTree2D):
     Klass = _PRTree3D
