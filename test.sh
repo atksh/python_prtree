5315d3fa8fd99e17a89beb699ba24f6e87e6917a
kota_ishizuka <ishizuka.k.ad@m.titech.ac.jp>
2021-07-25 18:06:00 +0900

    fix

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 set -e
 
 rm -rf build dist .pytest_cache
-pip install .
-pytest tests -vv --capture=no || rm -rf build dist .pytest_cache
+pip install . --force-reinstall
+python -m pytest tests -vv --capture=no || rm -rf build dist .pytest_cache
 
