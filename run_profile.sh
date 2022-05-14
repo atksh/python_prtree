e882afd7cdc9478e569dbecf12ac319188c67021
atksh <31435204+atksh@users.noreply.github.com>
2022-05-14 06:37:37 +0000

    fix

--- run_profile.sh
+++ run_profile.sh
@@ -3,7 +3,7 @@ set -e
 rm -rf build dist .pytest_cache
 pip uninstall python_prtree -y || true
 DEBUG=1 pip install -v -e .
-CPUPROFILE_FREQUENCY=10000 python docs/profile.py
+CPUPROFILE_FREQUENCY=10000 python docs/run_profile.py
 
 so_path=src/python_prtree/PRTree.cpython-310-x86_64-linux-gnu.so
 google-pprof --callgrind $so_path build.prof > cg_build.prof
