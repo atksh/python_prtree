365c8518c73e8b9b30d8a3587d2e778456f4e907
atksh <31435204+atksh@users.noreply.github.com>
2022-11-03 00:08:35 +0900

    update to v0.6.0

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from multiprocessing import cpu_count
 from setuptools import Extension, find_packages, setup
 from setuptools.command.build_ext import build_ext
 
-version = "v0.5.18"
+version = "v0.6.0"
 
 sys.path.append("./tests")
 
